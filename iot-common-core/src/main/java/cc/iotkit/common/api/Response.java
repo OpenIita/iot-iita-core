310c6f06839de59a4d084a694fa0235e67ddb6b9
jay <75509151@qq.com>
2024-05-11 18:44:01 +0800

    update Response

--- iot-common-core/src/main/java/cc/iotkit/common/api/Response.java
+++ iot-common-core/src/main/java/cc/iotkit/common/api/Response.java
@@ -23,6 +23,7 @@
 
 package cc.iotkit.common.api;
 
+import cn.hutool.core.util.IdUtil;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -30,9 +31,13 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Response {
+public class Response<T> {
     private Integer code;
     private String message;
     private Object data;
     private String requestId;
+
+   public static <T> Response success(T data) {
+        return new Response(200,"成功", data, IdUtil.simpleUUID());
+    }
 }
