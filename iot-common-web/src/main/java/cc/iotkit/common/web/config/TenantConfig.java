5027fbdaecf6593f0b0f14111c36533a5cc177bb
jay <75509151@qq.com>
2024-06-17 19:19:11 +0800

    del 租户配置

--- iot-common-web/src/main/java/cc/iotkit/common/web/config/TenantConfig.java
+++ iot-common-web/src/main/java/cc/iotkit/common/web/config/TenantConfig.java
@@ -1,45 +1,44 @@
-/*
- *
- *  * | Licensed 未经许可不能去掉「OPENIITA」相关版权
- *  * +----------------------------------------------------------------------
- *  * | Author: xw2sy@163.com
- *  * +----------------------------------------------------------------------
- *
- *  Copyright [2024] [OPENIITA]
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- * /
- */
-
-package cc.iotkit.common.web.config;
-
-import cc.iotkit.common.web.interceptor.TenantInterceptor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-/**
- * 注册租户ID拦截器.
- *
- * @author Tiger Chen
- * created on 2023/7/15 14:48
- */
-
-@Configuration
-public class TenantConfig implements WebMvcConfigurer {
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new TenantInterceptor());
-    }
-
-}
+///*
+// *
+// *  * | Licensed 未经许可不能去掉「OPENIITA」相关版权
+// *  * +----------------------------------------------------------------------
+// *  * | Author: xw2sy@163.com
+// *  * +----------------------------------------------------------------------
+// *
+// *  Copyright [2024] [OPENIITA]
+// *
+// *  Licensed under the Apache License, Version 2.0 (the "License");
+// *  you may not use this file except in compliance with the License.
+// *  You may obtain a copy of the License at
+// *
+// *     http://www.apache.org/licenses/LICENSE-2.0
+// *
+// *  Unless required by applicable law or agreed to in writing, software
+// *  distributed under the License is distributed on an "AS IS" BASIS,
+// *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// *  See the License for the specific language governing permissions and
+// *  limitations under the License.
+// * /
+// */
+//
+//package cc.iotkit.common.web.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+///**
+// * 注册租户ID拦截器.
+// *
+// * @author Tiger Chen
+// * created on 2023/7/15 14:48
+// */
+//
+//@Configuration
+//public class TenantConfig implements WebMvcConfigurer {
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(new TenantInterceptor());
+//    }
+//
+//}
